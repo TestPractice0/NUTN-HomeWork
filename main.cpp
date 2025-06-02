bb9d0c89fcdaf05a8884dde1ae56a1a264799be3
jack <S11259047@stumail.nutn.edu.tw>
2025-06-02 17:08:18 +0800

    ft:foo

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void foo() {
-
+    cout << "It¡¦s foo!!" << endl;
 }
 
 void bar() {
@@ -11,7 +11,8 @@ void bar() {
  
 int main() {
     cout << "Hello, World!" << endl;
+
+    foo();
     return 0;
 }
-
 
